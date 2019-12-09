7f2beaf64b905431bb6e7f47deb1e54e435e2392
Richard Carmona-Andrade <richardc97@ksu.edu>
2019-12-09 11:21:42 -0600

    Updates

--- Project8/Board.cs
+++ Project8/Board.cs
@@ -6,45 +6,133 @@ using System.Threading.Tasks;
 
 namespace Project8
 {
-    class Board
+
+    public enum PieceColor { empty, black, red };
+    public class Board
     {
-        public enum PieceColor { empty, black, red};
+        
+        private const int row = 6;
+        private const int Column = 7;
+        private PieceColor[,] grid;
+        //private PieceColor Turn;
 
-        public PieceColor[] grid = new PieceColor[6, 7];
+        public Board()
+        {
+            grid = new PieceColor[row, Column];
 
-        public PieceColor Turn { get; }
+            for(int i = 0; i <= row ; i++)
+            {
+                for(int j = 0; j <= Column ; j++)
+                {
+                    grid [i,j] = PieceColor.empty;
+                }
+            }
+    }
+
+        public PieceColor Turn { get { return Turn; } }
         
         public PieceColor GetColor(int x, int y)
         {
-            if([x,y] == PieceColor.black)
-            {
-                return PieceColor.black;
-                PieceColor.black;
-            } 
-            
+            return grid[x, y];
         }
 
         public bool IsWinner(PieceColor player)
         {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < Column; j++){ 
+                if (grid[i,j] == grid[i, j+1] && grid[i,j] == grid[i,j +2] && grid[i,j] == grid[i,j + 3] && grid[i,j] == player)
+                {
+                return true;
+                }
+            else
+                {
+                return false;
+                }
+                }
+            }
 
+            for (int j = 0; j < Column; j++)
+            {
+                for (int i = 0; i < row - 3; i++)
+                {
+                    if (grid[i, j] == grid[i, j + 1] && grid[i, j] == grid[i, j + 2] && grid[i, j] == grid[i, j + 3] && grid[i, j] == player)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < Column - 4; j++)
+                {
+                    if (grid[i, j] == grid[i, j + 1] && grid[i, j] == grid[i, j + 2] && grid[i, j] == grid[i, j + 3] && grid[i, j] == player)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
 
         public bool CheckTie()
         {
-
+            for(int i = 0; i < row; i++)
+            {
+                for(int j = 0; j < Column; j++)
+                {
+                    if (grid[i,j] != PieceColor.empty)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+            return true;
         }
 
         public void SwitchTurns()
         {
+            if(Turn == PieceColor.black) Turn = PieceColor.red;
+            
+            else Turn = PieceColor.black;
+            
+        }
 
+        private bool IsValidMove(int col)
+        {
+            if (grid[0,col] == PieceColor.empty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool Move(int col)
         {
-
+            if (IsValidMove(col)) {
+                return true;
         }
-
+            else
+            {
+                return false;
+            }
+        }
     }
-    public enum PieceColor { empty, red, black };
-
 }
